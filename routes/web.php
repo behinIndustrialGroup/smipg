e72a640cae315db4850870cf8743865aaa1a4e7d
mkhodroo <mkhodroo@yahoo.com>
2024-03-09 19:23:52 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/migrate', function () {
-    Artisan::call('config:clear');
+    Artisan::call('storage:link');
     Artisan::call('migrate');
 });
 
