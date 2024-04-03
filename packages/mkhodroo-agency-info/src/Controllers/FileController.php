fb12723c3edf03298efa66d2a6e28d9151833ebf
mkhodroo <mkhodroo@yahoo.com>
2024-04-03 10:24:19 +0330

    Update FileController.php

--- packages/mkhodroo-agency-info/src/Controllers/FileController.php
+++ packages/mkhodroo-agency-info/src/Controllers/FileController.php
@@ -20,7 +20,7 @@ class FileController extends Controller
         $full_path = public_path($dir);
         Log::info($full_path);
         if ( !is_dir($full_path)) {
-            mkdir($full_path, 0644, false);
+            mkdir($full_path);
         }
         $full_name = $full_path . '/' . $name;
         $result = move_uploaded_file($file,$full_name);
