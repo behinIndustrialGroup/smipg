4792d224e0d0ac95fc3b2fe7a33b13e8b3dff82e
mkhodroo <mkhodroo@yahoo.com>
2024-04-13 18:54:26 +0330

    fix migration

--- packages/mkhodroo-agency-info/src/Migrations/2023_10_15_080057_add_soft_delete_to_agency_info_table.php
+++ packages/mkhodroo-agency-info/src/Migrations/2023_10_15_080057_add_soft_delete_to_agency_info_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('agency_info', function (Blueprint $table) {
-            $table->softDeletes();
+            // $table->softDeletes();
         });
     }
 
