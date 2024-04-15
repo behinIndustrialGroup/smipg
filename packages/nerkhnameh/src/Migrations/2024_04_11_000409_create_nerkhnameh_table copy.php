190de472669191f004ff2e1ceee45efbb9a9b50b
mkhodroo <mkhodroo@yahoo.com>
2024-04-15 09:26:52 +0330

    Update 2024_04_11_000409_create_nerkhnameh_table copy.php

--- packages/nerkhnameh/src/Migrations/2024_04_11_000409_create_nerkhnameh_table copy.php
+++ packages/nerkhnameh/src/Migrations/2024_04_11_000409_create_nerkhnameh_table copy.php
@@ -11,23 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('nerkhnameh', function (Blueprint $table) {
-            // $table->id();
-            // $table->string('guild_name')->nullable();
-            // $table->string('fullname')->nullable();
-            // $table->string('national_id')->nullable();
-            // $table->string('mobile')->nullable();
-            // $table->string('tel')->nullable();
-            // $table->string('guild_number')->nullable();
-            // $table->text('address')->nullable();
-            // $table->string('personal_image_file')->nullable();
-            // $table->string('commitment_file')->nullable();
-            // $table->string('price')->nullable();
-            // $table->string('price_payment_file')->nullable();
-            // $table->tinyInteger('fin_validation')->nullable();
-            // $table->string('nerkhnameh_file')->nullable();
-            // $table->timestamps();
-        });
+        // Schema::create('nerkhnameh', function (Blueprint $table) {
+        //     // $table->id();
+        //     // $table->string('guild_name')->nullable();
+        //     // $table->string('fullname')->nullable();
+        //     // $table->string('national_id')->nullable();
+        //     // $table->string('mobile')->nullable();
+        //     // $table->string('tel')->nullable();
+        //     // $table->string('guild_number')->nullable();
+        //     // $table->text('address')->nullable();
+        //     // $table->string('personal_image_file')->nullable();
+        //     // $table->string('commitment_file')->nullable();
+        //     // $table->string('price')->nullable();
+        //     // $table->string('price_payment_file')->nullable();
+        //     // $table->tinyInteger('fin_validation')->nullable();
+        //     // $table->string('nerkhnameh_file')->nullable();
+        //     // $table->timestamps();
+        // });
     }
 
     /**
