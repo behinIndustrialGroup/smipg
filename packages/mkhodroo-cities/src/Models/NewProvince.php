70bb25432f052b472529cc0fbb819fc1bc8ec7ff
mkhodroo <info@behin.energy>
2024-07-30 23:31:37 +0330

    Update NewProvince.php

--- packages/mkhodroo-cities/src/Models/NewProvince.php
+++ packages/mkhodroo-cities/src/Models/NewProvince.php
@@ -3,9 +3,11 @@
 namespace Mkhodroo\Cities\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class NewProvince extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'name'
     ];
