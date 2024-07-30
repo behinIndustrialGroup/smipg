754ef8ca822a0ff86b9fd88621b15b1957e83f5e
mkhodroo <info@behin.energy>
2024-07-30 23:12:45 +0330

    Update QueryController.php

--- packages/mkhodroo-agency-info/src/Controllers/QueryController.php
+++ packages/mkhodroo-agency-info/src/Controllers/QueryController.php
@@ -12,25 +12,25 @@ class QueryController extends Controller
 {
     public static function agencyEditor()
     {
-        $cities = City::all()->groupBy('province');
-        foreach($cities as $province => $city){
-            NewProvince::create([
-                'name' => $province
-            ]);
-        }
-        $cities = City::all();
-        $provinces = NewProvince::all();
-        $agencies = AgencyInfo::where('key', 'province')->get();
-        foreach ($agencies as $agency) {
-            $province_name = $cities->where('id', $agency->value)->first()->province;
-            // echo $provinces->where('name', $province_name)->first()->id . '<br>';
-            AgencyInfo::create([
-                'key' => 'province',
-                'value' => $provinces->where('name', $province_name)->first()->id,
-                'parent_id' => $agency->parent_id
-            ]);
-            $agency->update(['key' => 'city']);
-        }
-        return true;
+        // $cities = City::all()->groupBy('province');
+        // foreach($cities as $province => $city){
+        //     NewProvince::create([
+        //         'name' => $province
+        //     ]);
+        // }
+        // $cities = City::all();
+        // $provinces = NewProvince::all();
+        // $agencies = AgencyInfo::where('key', 'province')->get();
+        // foreach ($agencies as $agency) {
+        //     $province_name = $cities->where('id', $agency->value)->first()->province;
+        //     // echo $provinces->where('name', $province_name)->first()->id . '<br>';
+        //     AgencyInfo::create([
+        //         'key' => 'province',
+        //         'value' => $provinces->where('name', $province_name)->first()->id,
+        //         'parent_id' => $agency->parent_id
+        //     ]);
+        //     $agency->update(['key' => 'city']);
+        // }
+        // return true;
     }
 }
