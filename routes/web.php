4af5c7eb191758a43bbd4de66a05cf33e2e53b0f
behinIndustrialGroup <info@behin.energy>
2025-10-07 12:12:48 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/migrate', function () {
+    Artisan::call('config:cache');
     Artisan::call('config:clear');
+    Artisan::call('cache:clear');
     Artisan::call('migrate');
 });
 
