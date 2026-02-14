972c718017df28386597ee94359d6cbc81507f60
behinIndustrialGroup <info@behin.energy>
2026-02-14 20:42:23 +0330

    Update index.blade.php

--- packages/MarketingCard/resources/views/index.blade.php
+++ packages/MarketingCard/resources/views/index.blade.php
@@ -1,39 +1,44 @@
 @extends('marketingcard::layouts.master')
 
 @section('content')
-    <div class="row table-responsive">
-        <a href="{{ route('marketingcard.create') }}" class="btn btn-primary">{{ trans('marketingTrans::msg.create') }}</a>
+    <div class="card">
+        <div class="row table-responsive">
+            <a href="{{ route('marketingcard.create') }}"
+                class="btn btn-primary">{{ trans('marketingTrans::msg.create') }}</a>
 
-        <table class="table table-bordered" id="peopleTable">
-            <thead>
-                <tr>
-                    <th>{{ trans('marketingTrans::msg.national_code') }}</th>
-                    <th>{{ trans('marketingTrans::msg.first_name') }}</th>
-                    <th>{{ trans('marketingTrans::msg.last_name') }}</th>
-                    <th>{{ trans('marketingTrans::msg.father_name') }}</th>
-                    <th>{{ trans('marketingTrans::msg.issue_date') }}</th>
-                    <th>{{ trans('marketingTrans::msg.expiry_date') }}</th>
-                    <th>{{ trans('marketingTrans::msg.show') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($people as $person)
+            <table class="table table-bordered" id="peopleTable">
+                <thead>
                     <tr>
-                        <td>{{ $person->nationalId }}</td>
-                        <td>{{ $person->firstName }}</td>
-                        <td>{{ $person->lastName }}</td>
-                        <td>{{ $person->fatherName }}</td>
-                        <td>{{ $person->issueDate('persian') }}</td>
-                        <td>{{ $person->expiryDate('persian') }}</td>
-                        <td><a
-                                href="{{ route('marketingcard.edit', ['marketingcard' => $person->id]) }}">{{ trans('marketingTrans::msg.show') }}</a>
-                        </td>
+                        <th>کدملی</th>
+                        <th>نام</th>
+                        <th>نام خانوادگی</th>
+                        <th>نام پدر</th>
+                        <th>تاریخ صدور</th>
+                        <th>تاریخ انقضا</th>
+                        <th>اقدام</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($people as $person)
+                        <tr>
+                            <td>{{ $person->nationalId }}</td>
+                            <td>{{ $person->firstName }}</td>
+                            <td>{{ $person->lastName }}</td>
+                            <td>{{ $person->fatherName }}</td>
+                            <td>{{ $person->issueDate('persian') }}</td>
+                            <td>{{ $person->expiryDate('persian') }}</td>
+                            <td><a
+                                    href="{{ route('marketingcard.edit', ['marketingcard' => $person->id]) }}">
+                                    <i class="fa fa-edit"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
 
 
+        </div>
     </div>
 @endsection
 
